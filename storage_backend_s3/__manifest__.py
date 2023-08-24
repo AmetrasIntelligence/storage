e267850c60b80da2c7bc9e1873c2dd434df40a08
Bastian Guenther <Bastian.Guenther@ametras.com>
2023-08-24 17:45:02 +0200

    [FIX] storage_backend_s3: Added missing dependencies

--- storage_backend_s3/__manifest__.py
+++ storage_backend_s3/__manifest__.py
@@ -11,7 +11,7 @@
     "author": " Akretion, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "external_dependencies": {"python": ["boto3"]},
+    "external_dependencies": {"python": ["boto3", "vcrpy-unittest", "requests-mock"]},
     "depends": ["storage_backend"],
     "data": ["views/backend_storage_view.xml"],
 }
